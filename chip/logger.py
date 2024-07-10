d8b3a0ca535d8ff36213e7b4e75d9a5055ca3071
Davide Nale <davide.nale.99@gmail.com>
2024-07-10 18:02:00 +0200

    Added logger

---
+++ chip/logger.py
@@ -0,0 +1,30 @@
+"""Logger."""
+
+import logging
+
+import structlog
+
+
+def configure() -> None:
+    """Configure structlog."""
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.StackInfoRenderer(),
+            structlog.dev.set_exc_info,
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
+            structlog.dev.ConsoleRenderer(),
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=False,
+    )
+
+def new() -> structlog.BoundLogger:
+    """Return a new configured logger."""
+    return structlog.get_logger()
+
+# Run configure when loading the module
+configure()
