1ad130955619c5df15bce423e3907b17f59f0f34
Davide Nale <davide.nale.99@gmail.com>
2024-07-10 21:28:01 +0200

    Updated example and pyproject

--- example/example.py
+++ example/example.py
@@ -1,14 +1,20 @@
 """Example usage of the chip package."""
 
+from typing import List
+
 import chip
 
-state = chip.State(
-        
-)
+
+# @chip.stateless @chip.statefull
+class State(chip.State):  # noqa: D101
+    history: List[str]
+
+
+state = State()
 
 chatbot = chip.Chatbot(
-    tasks=[],
     state=state,
+    tasks=[],
     logger=chip.logger.new(),
     debug=True,
 )
